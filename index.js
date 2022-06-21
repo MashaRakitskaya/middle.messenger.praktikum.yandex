650d3a1fb6082fb5e8ef13fc8be76a549f349128
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-06-21 19:27:38 +0200

    add: parcel, handlebars, scss, .gitignore, favicone

---
+++ index.js
@@ -0,0 +1,15 @@
+import template from "./src/components/component.js";
+import "./src/styles/index.scss";
+
+const root = document.getElementById("root");
+const path = window.location.pathname;
+
+function replacePath() {
+  return window.location.replace("http://localhost:3000/signin");
+}
+
+if (path === "/") {
+  replacePath();
+} else if (path === "/signin") {
+  root.innerHTML = template;
+}
