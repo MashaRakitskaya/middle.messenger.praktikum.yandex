ced2c8d9a57d096ed0e30915b6560a1eee9d9e5d
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-06-22 16:48:51 +0200

    add: form modul

--- src/modules/form/form.js
+++ src/modules/form/form.js
@@ -2,5 +2,7 @@ import Handlebars from "handlebars";
 import "../../components/input/input.js";
 import form from "./form.hbs";
 import "./form.scss";
+import "../../components/pageTitle/pageTitle.js";
+import "../../components/formButton/formButton.js";
 
 Handlebars.registerPartial("formPartial", form);
