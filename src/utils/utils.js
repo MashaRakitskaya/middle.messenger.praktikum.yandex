d0c6fe7c24be10f4c5a07ffc36d36f2899aa7bc0
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-06-22 22:22:37 +0200

    add: utils PROFILE_SETTING_PATH = '/profile-setting'; PASSWORD_SETTING_PATH = '/password-setting';

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,3 +1,5 @@
 export const BASE_URL = window.origin;
 export const SIGNIN_PATH = "/signin";
 export const SIGNUP_PATH = "/signup";
+export const PROFILE_SETTING_PATH = '/profile-setting';
+export const PASSWORD_SETTING_PATH = '/password-setting';
