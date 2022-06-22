879ea4b15a7fdc7b2d6293c2657fe999704c4965
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-06-22 16:55:40 +0200

    add:  BASE_URL, SIGNIN_PATH, SIGNUP_PATH

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-// import template from "./src/components/component.js";
 import "./src/styles/index.scss";
+import { BASE_URL, SIGNIN_PATH, SIGNUP_PATH } from "./src/utils/utils";
 import signin from "./src/pages/signin/signin.js";
+import signup from "./src/pages/signup/signup.js";
 
 const root = document.getElementById("root");
 const path = window.location.pathname;
 
 function replacePath() {
-  return window.location.replace("http://localhost:3000/signin");
+  return window.location.replace(`${BASE_URL}${SIGNIN_PATH}`);
 }
 
 if (path === "/") {
   replacePath();
-} else if (path === "/signin") {
+} else if (path === SIGNIN_PATH) {
   root.innerHTML = signin;
+} else if (path === SIGNUP_PATH) {
+  root.innerHTML = signup;
 }
