a4450ed6936d66480919822bd4c530d19827a95b
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-06-22 22:20:37 +0200

    add: index.js PROFILE_SETTING_PATH,   PASSWORD_SETTING_PATH,

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import "./src/styles/index.scss";
-import { BASE_URL, SIGNIN_PATH, SIGNUP_PATH } from "./src/utils/utils";
+import {
+  BASE_URL,
+  SIGNIN_PATH,
+  SIGNUP_PATH,
+  PROFILE_SETTING_PATH,
+  PASSWORD_SETTING_PATH,
+} from "./src/utils/utils";
 import signin from "./src/pages/signin/signin.js";
 import signup from "./src/pages/signup/signup.js";
+import profileSetting from "./src/pages/profileSetting/profileSetting.js";
+import passwordSetting from "./src/pages/passwordSetting/passwordSetting.js";
 
 const root = document.getElementById("root");
 const path = window.location.pathname;
@@ -16,4 +24,8 @@ if (path === "/") {
   root.innerHTML = signin;
 } else if (path === SIGNUP_PATH) {
   root.innerHTML = signup;
+} else if (path === PROFILE_SETTING_PATH) {
+  root.innerHTML = profileSetting;
+} else if (path === PASSWORD_SETTING_PATH) {
+  root.innerHTML = passwordSetting;
 }
