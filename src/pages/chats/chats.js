20a68bbe87e364ca0e1fcaf29127c6d9ba419cf3
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-05 21:47:20 +0200

    add: chats page

--- src/pages/chats/chats.js
+++ src/pages/chats/chats.js
@@ -1,4 +1,123 @@
 import "./chats.scss";
+import "../../modules/chatsList/chatsList.js";
 import chats from "./chats.hbs";
+import "../../components/inputSearch/inputSearch.js";
 
-export default chats();
+const data = {
+  isChatSelected: true,
+  inputSearchContext: {
+    inputName: "search",
+  },
+  chats: [
+    {
+      chatAvatarContext: {
+        urlImg:
+          "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      },
+      chatContext: {
+        chatName: "Elena List",
+        chatMessage: "hat are extremely painful. Nor aga...",
+        time: "12:00",
+        numberMessages: "2344",
+      },
+    },
+    {
+      chatAvatarContext: {
+        urlImg:
+          "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      },
+      chatContext: {
+        chatName: "Elena List",
+        chatMessage: "hat are extremely painful. Nor aga...",
+        time: "12:00",
+        numberMessages: "2344",
+      },
+    },
+    {
+      chatAvatarContext: {
+        urlImg:
+          "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      },
+      chatContext: {
+        chatName: "Elena List",
+        chatMessage: "hat are extremely painful. Nor aga...",
+        time: "12:00",
+        numberMessages: "2344",
+      },
+    },
+    {
+      chatAvatarContext: {
+        urlImg:
+          "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      },
+      chatContext: {
+        chatName: "Elena List",
+        chatMessage: "hat are extremely painful. Nor aga...",
+        time: "12:00",
+        numberMessages: "2344",
+      },
+    },
+    {
+      chatAvatarContext: {
+        urlImg:
+          "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      },
+      chatContext: {
+        chatName: "Elena List",
+        chatMessage: "hat are extremely painful. Nor aga...",
+        time: "12:00",
+        numberMessages: "2344",
+      },
+    },
+    {
+      chatAvatarContext: {
+        urlImg:
+          "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      },
+      chatContext: {
+        chatName: "Elena List",
+        chatMessage: "hat are extremely painful. Nor aga...",
+        time: "12:00",
+        numberMessages: "2344",
+      },
+    },
+    {
+      chatAvatarContext: {
+        urlImg:
+          "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      },
+      chatContext: {
+        chatName: "Elena List",
+        chatMessage: "hat are extremely painful. Nor aga...",
+        time: "12:00",
+        numberMessages: "2344",
+      },
+    },
+    {
+      chatAvatarContext: {
+        urlImg:
+          "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      },
+      chatContext: {
+        chatName: "Elena List",
+        chatMessage: "hat are extremely painful. Nor aga...",
+        time: "12:00",
+        numberMessages: "2344",
+      },
+    },
+    {
+      chatAvatarContext: {
+        urlImg:
+          "https://images.unsplash.com/photo-1656931251449-07493b9f6caf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      },
+      chatContext: {
+        chatName: "Elena List",
+        chatMessage: "hat are extremely painful. Nor aga...",
+        time: "12:00",
+        numberMessages: "2344",
+      },
+    },
+  ],
+};
+
+export default chats(data);
