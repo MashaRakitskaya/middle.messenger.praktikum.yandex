629eae0506ac50ad18b2b25536978a625eae6fd2
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-09 13:51:15 +0200

    change: error404.hbs.ts create class Error404 extends Block

--- src/pages/error404/error404.ts
+++ src/pages/error404/error404.ts
@@ -1,12 +1,24 @@
-import "../../modules/error/error.ts";
-import { BASE_URL, CHATS_PATH } from "../../utils/utils.ts";
-import error404 from "./error404.hbs";
+import Block from "../../common/Block/Block";
+import { BASE_URL, CHATS_PATH } from "../../utils/utils";
+import error404Template from "./error404.hbs";
+import Error from "../../modules/error/error";
 import "./error404.scss";
+import { renderDom } from "../../utils/renderDOM";
 
-const data = {
-  errorTitle: "404",
-  errorText: "Page not found",
-  url: `${BASE_URL}${CHATS_PATH}`,
-};
+class Error404 extends Block {
+  constructor(props = {}) {
+    const error = new Error({
+      errorTitle: "404",
+      errorText: "Page not found",
+      url: `${BASE_URL}${CHATS_PATH}`,
+    });
 
-export default error404(data);
+    super("div", { ...props, error });
+  }
+
+  render() {
+    return this.compile(error404Template, this.props);
+  }
+}
+
+export default Error404;
