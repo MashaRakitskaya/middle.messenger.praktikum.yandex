30298c424ebeadfaa0c9631ed21b1caa7b414a6c
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-10 12:40:45 +0200

    change: class Signin

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -2,51 +2,39 @@ import Block from "../../common/Block/Block";
 import { BASE_URL, SIGNUP_PATH } from "../../utils/utils.ts";
 import "./signin.scss";
 import signinTemplate from "./signin.hbs";
-import Form from "../../modules/form/form";
 import PageTitle from "../../components/pageTitle/pageTitle";
 import FormButton from "../../components/formButton/formButton";
 import Input from "../../components/input/input";
-
-const inputsArrey = [
-  {
-    input: {
-      name: "login",
-      label: "login",
-      type: "text",
-    },
-  },
-  {
-    input: {
-      name: "password",
-      label: "password",
-      type: "password",
-    },
-  },
-];
 
 class Signin extends Block {
   constructor(props = {}) {
     const pageTitle = new PageTitle({ pageTitle: "Login" });
 
-    // const inputs = inputsArrey.map((item) => {
-    //   return { input: new Input(item) };
-    // });
+    const inputLogin = new Input({
+      name: "login",
+      label: "login",
+      type: "text",
+    });
 
-    const inputs = inputsArrey.map((item) => new Input(item));
+    const inputPassword = new Input({
+      name: "password",
+      label: "password",
+      type: "password",
+    });
 
     const formButton = new FormButton({
       buttonText: "Sign in",
     });
 
-    const form = new Form({
-      isLink: true,
+    super("div", {
+      ...props,
+      pageTitle,
+      inputLogin,
+      inputPassword,
+      formButton,
       linkText: "Sign up",
       url: `${BASE_URL}${SIGNUP_PATH}`,
-      pageTitle,
-      inputs,
-      formButton,
     });
-    super("div", { ...props, form });
   }
 
   render() {
