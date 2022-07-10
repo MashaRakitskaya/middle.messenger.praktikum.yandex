f9dd0a303421e2db98eda18ec89108505fb11fc0
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-10 12:40:28 +0200

    change: class Signup

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -2,84 +2,73 @@ import Block from "../../common/Block/Block";
 import { BASE_URL, SIGNIN_PATH } from "../../utils/utils.ts";
 import "./signup.scss";
 import signupTemplate from "./signup.hbs";
-import Form from "../../modules/form/form";
 import PageTitle from "../../components/pageTitle/pageTitle";
 import FormButton from "../../components/formButton/formButton";
 import Input from "../../components/input/input";
-
-const inputsArrey = [
-  {
-    input: {
-      name: "email",
-      label: "email",
-      type: "email",
-    },
-  },
-  {
-    input: {
-      name: "login",
-      label: "login",
-      type: "text",
-    },
-  },
-  {
-    input: {
-      name: "first_name",
-      label: "name",
-      type: "text",
-    },
-  },
-  {
-    input: {
-      name: "second_name",
-      label: "surename",
-      type: "text",
-    },
-  },
-  {
-    input: {
-      name: "phone",
-      label: "phone number",
-      type: "tel",
-    },
-  },
-  {
-    input: {
-      name: "password",
-      label: "password",
-      type: "password",
-    },
-  },
-  {
-    input: {
-      name: "password (again)",
-      label: "password (again)",
-      type: "password",
-    },
-  },
-];
-
 class Signup extends Block {
   constructor(props = {}) {
     const pageTitle = new PageTitle({ pageTitle: "Sign up" });
 
-    const inputs = inputsArrey.map((item) => {
-      return { input: new Input(item) };
+    const inputEmail = new Input({
+      name: "email",
+      label: "email",
+      type: "email",
+    });
+
+    const inputLogin = new Input({
+      name: "login",
+      label: "login",
+      type: "text",
+    });
+
+    const inputFirstName = new Input({
+      name: "first_name",
+      label: "name",
+      type: "text",
+    });
+
+    const inputSecondName = new Input({
+      name: "second_name",
+      label: "surename",
+      type: "text",
+    });
+
+    const inputPhone = new Input({
+      name: "phone",
+      label: "phone number",
+      type: "tel",
+    });
+
+    const inputPassword = new Input({
+      name: "password",
+      label: "password",
+      type: "password",
+    });
+
+    const inputPasswordAgain = new Input({
+      name: "password (again)",
+      label: "password (again)",
+      type: "password",
     });
 
     const formButton = new FormButton({
       buttonText: "Sign up",
     });
 
-    const form = new Form({
-      isLink: true,
+    super("div", {
+      ...props,
+      pageTitle,
+      inputEmail,
+      inputLogin,
+      inputFirstName,
+      inputSecondName,
+      inputPhone,
+      inputPassword,
+      inputPasswordAgain,
+      formButton,
       linkText: "Sign in",
       url: `${BASE_URL}${SIGNIN_PATH}`,
-      pageTitle,
-      inputs,
-      formButton,
     });
-    super("div", { ...props, form });
   }
 
   render() {
