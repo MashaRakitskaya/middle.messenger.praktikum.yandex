4441b390518528842718d9728119fdbe00726295
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-10 17:44:04 +0200

    add: class Sibebar

--- src/modules/sidebar/sidebar.ts
+++ src/modules/sidebar/sidebar.ts
@@ -1,7 +1,52 @@
-import Handlebars from "handlebars";
 import "./sidebar.scss";
-import "../../components/inputSearch/inputSearch.ts";
-import "../../modules/chatsList/chatsList.ts";
-import sidebar from "./sidebar.hbs";
+import sidebarTemplate from "./sidebar.hbs";
+import Block from "../../common/Block/Block";
+import InputSearch from "../../components/inputSearch/inputSearch";
+import Chat from "../../components/chat/chat";
 
-Handlebars.registerPartial("sidebarPartial", sidebar);
+class Sidebar extends Block {
+  constructor(props = {}) {
+    const inputSearch = new InputSearch({ inputName: "search" });
+
+    const chat1 = new Chat({
+      urlImg:
+        "https://images.unsplash.com/photo-1655269359642-caf63bad5a05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      chatName: "Elena List",
+      chatMessage: "hat are extremely painful. Nor aga...",
+      time: "12:00",
+      numberMessages: "2344",
+    });
+
+    const chat2 = new Chat({
+      urlImg:
+        "https://images.unsplash.com/photo-1655269359642-caf63bad5a05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      chatName: "Elena List",
+      chatMessage: "hat are extremely painful. Nor aga...",
+      time: "12:00",
+      numberMessages: "2344",
+    });
+
+    const chat3 = new Chat({
+      urlImg:
+        "https://images.unsplash.com/photo-1655269359642-caf63bad5a05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
+      chatName: "Elena List",
+      chatMessage: "hat are extremely painful. Nor aga...",
+      time: "12:00",
+      numberMessages: "2344",
+    });
+
+    super("div", {
+      ...props,
+      inputSearch,
+      chat1,
+      chat2,
+      chat3,
+    });
+  }
+
+  render() {
+    return this.compile(sidebarTemplate, this.props);
+  }
+}
+
+export default Sidebar;
