e9134f9ecfaf2f90a38b346c9a2f031dfb53cbc2
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-12 13:10:31 +0200

    add: utils.ts const inputsProperties

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -7,3 +7,43 @@ export const CHATS_PATH = "/chats";
 export const ERROR404_PATH = "/404";
 export const ERROR500_PATH = "/500";
 export const rootDiv = "#root";
+
+export const inputsProperties = {
+  login: {
+    name: "login",
+    label: "login",
+    type: "text",
+  },
+  password: {
+    name: "password",
+    label: "password",
+    type: "password",
+  },
+  password_again: {
+    name: "password (again)",
+    label: "password (again)",
+    type: "password",
+  },
+  email: {
+    name: "email",
+    label: "email",
+    type: "email",
+  },
+  first_name: {
+    name: "first_name",
+    label: "name",
+    type: "text",
+  },
+  second_name: {
+    name: "second_name",
+    label: "surename",
+    type: "text",
+  },
+  phone: {
+    name: "phone",
+    label: "phone number",
+    type: "tel",
+  },
+};
+
+
