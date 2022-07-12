e11ac1b9399e7dc44a5b6531ba3f718fb9c30ce9
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-12 13:05:43 +0200

    add: signin form validation

--- src/pages/signin/signin.ts
+++ src/pages/signin/signin.ts
@@ -5,25 +5,79 @@ import signinTemplate from "./signin.hbs";
 import PageTitle from "../../components/pageTitle/pageTitle";
 import FormButton from "../../components/formButton/formButton";
 import Input from "../../components/input/input";
+import { inputsProperties } from "../../utils/utils";
+import {
+  getformData,
+  inputIsNotValid,
+  onBlur,
+  onFocus,
+  validationMessageAndRegExp,
+} from "../../utils/validation";
 
 class Signin extends Block {
   constructor(props = {}) {
+    const { login, password } = inputsProperties;
     const pageTitle = new PageTitle({ pageTitle: "Login" });
 
     const inputLogin = new Input({
-      name: "login",
-      label: "login",
-      type: "text",
+      ...login,
+      events: {
+        blur: (event) => {
+          onBlur({
+            target: event.target,
+            value: event.target.value,
+            input: login.name,
+          });
+        },
+        focus: (event) => {
+          onFocus({ target: event.target });
+        },
+      },
     });
 
     const inputPassword = new Input({
-      name: "password",
-      label: "password",
-      type: "password",
+      ...password,
+      events: {
+        blur: (event) => {
+          onBlur({
+            target: event.target,
+            value: event.target.value,
+            input: password.name,
+          });
+        },
+        focus: (event) => {
+          onFocus({ target: event.target });
+        },
+      },
     });
 
     const formButton = new FormButton({
       buttonText: "Sign in",
+      events: {
+        click: (event) => {
+          event.preventDefault();
+          const formElement = document.querySelector("form") as HTMLFormElement;
+          const inputs = formElement.querySelectorAll("input");
+          const inputLoginTarget = inputs[0];
+          const inputPasswordTarget = inputs[1];
+
+          inputIsNotValid({
+            input: validationMessageAndRegExp.login,
+            target: inputLoginTarget,
+            value: inputLoginTarget.value,
+            message: validationMessageAndRegExp.login.message,
+          });
+
+          inputIsNotValid({
+            input: validationMessageAndRegExp.password,
+            target: inputPasswordTarget,
+            value: inputPasswordTarget.value,
+            message: validationMessageAndRegExp.password.message,
+          });
+
+          getformData();
+        },
+      },
     });
 
     super("div", {
@@ -34,6 +88,8 @@ class Signin extends Block {
       formButton,
       linkText: "Sign up",
       url: `${BASE_URL}${SIGNUP_PATH}`,
+      nameInputLogin: login.name,
+      nameInputPassword: password.name,
     });
   }
 
