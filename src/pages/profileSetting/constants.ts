b0306423821c9e8dfc816c5ffe4f1299f2f946d9
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-12 17:22:07 +0200

    add: profileSetting validation

---
+++ src/pages/profileSetting/constants.ts
@@ -0,0 +1,19 @@
+import { inputsProperties } from "../../utils/utils";
+
+export const inputsNames = {
+  nameInputEmail: inputsProperties.email.name,
+  nameInputLogin: inputsProperties.login.name,
+  nameInputFirstName: inputsProperties.first_name.name,
+  nameInputSecondName: inputsProperties.second_name.name,
+  nameInputdisplayName: inputsProperties.display_name.name,
+  nameInputPhone: inputsProperties.phone.name,
+};
+
+export const inputsLabels = {
+  labelInputEmail: inputsProperties.email.label,
+  labelInputLogin: inputsProperties.login.label,
+  labelInputFirstName: inputsProperties.first_name.label,
+  labelInputSecondName: inputsProperties.second_name.label,
+  labelInputdisplayName: inputsProperties.display_name.label,
+  labelInputPhone: inputsProperties.phone.label,
+};
--- src/pages/profileSetting/profileSetting.ts
+++ src/pages/profileSetting/profileSetting.ts
@@ -6,49 +6,171 @@ import PageTitle from "../../components/pageTitle/pageTitle";
 import FormButton from "../../components/formButton/formButton";
 import Back from "../../components/back/back";
 import Input from "../../components/input/input";
+import { inputsProperties } from "../../utils/utils";
+import { inputsLabels, inputsNames } from "./constants";
+import {
+  getformData,
+  inputIsNotValid,
+  onBlur,
+  onFocus,
+  validationMessageAndRegExp,
+} from "../../utils/validation";
 
 class ProfileSetting extends Block {
   constructor(props = {}) {
+    const { email, login, first_name, second_name, phone, display_name } =
+      inputsProperties;
     const back = new Back();
     const pageTitle = new PageTitle({
       pageTitle: "Сhange profile data",
     });
-    const formButton = new FormButton({ buttonText: "Save" });
+    const formButton = new FormButton({
+      buttonText: "Save",
+      events: {
+        click: (event) => {
+          event.preventDefault();
+          const formElement = document.querySelector("form") as HTMLFormElement;
+          const inputs = formElement.querySelectorAll("input");
+          const inputEmailTarget = inputs[0];
+          const inputLoginTarget = inputs[1];
+          const inputFirstNameTarget = inputs[2];
+          const inputSecondNameTarget = inputs[3];
+          const inputPhoneTarget = inputs[5];
+
+          inputIsNotValid({
+            input: validationMessageAndRegExp.email,
+            target: inputEmailTarget,
+            value: inputEmailTarget.value,
+            message: validationMessageAndRegExp.email.message,
+          });
+
+          inputIsNotValid({
+            input: validationMessageAndRegExp.login,
+            target: inputLoginTarget,
+            value: inputLoginTarget.value,
+            message: validationMessageAndRegExp.login.message,
+          });
+
+          inputIsNotValid({
+            input: validationMessageAndRegExp.first_name,
+            target: inputFirstNameTarget,
+            value: inputFirstNameTarget.value,
+            message: validationMessageAndRegExp.first_name.message,
+          });
+
+          inputIsNotValid({
+            input: validationMessageAndRegExp.second_name,
+            target: inputSecondNameTarget,
+            value: inputSecondNameTarget.value,
+            message: validationMessageAndRegExp.second_name.message,
+          });
+
+          inputIsNotValid({
+            input: validationMessageAndRegExp.phone,
+            target: inputPhoneTarget,
+            value: inputPhoneTarget.value,
+            message: validationMessageAndRegExp.phone.message,
+          });
+
+          getformData('form');
+        },
+      },
+    });
 
     const inputEmail = new Input({
-      name: "email",
-      label: "email",
-      type: "email",
+      ...email,
+      events: {
+        blur: (event) => {
+          onBlur({
+            target: event.target,
+            value: event.target.value,
+            input: email.name,
+          });
+        },
+        focus: (event) => {
+          onFocus({ target: event.target });
+        },
+      },
     });
 
     const inputLogin = new Input({
-      name: "login",
-      label: "login",
-      type: "text",
+      ...login,
+      events: {
+        blur: (event) => {
+          onBlur({
+            target: event.target,
+            value: event.target.value,
+            input: login.name,
+          });
+        },
+        focus: (event) => {
+          onFocus({ target: event.target });
+        },
+      },
     });
 
     const inputFirstName = new Input({
-      name: "first_name",
-      label: "name",
-      type: "text",
+      ...first_name,
+      events: {
+        blur: (event) => {
+          onBlur({
+            target: event.target,
+            value: event.target.value,
+            input: first_name.name,
+          });
+        },
+        focus: (event) => {
+          onFocus({ target: event.target });
+        },
+      },
     });
 
     const inputSecondName = new Input({
-      name: "second_name",
-      label: "surename",
-      type: "text",
+      ...second_name,
+      events: {
+        blur: (event) => {
+          onBlur({
+            target: event.target,
+            value: event.target.value,
+            input: second_name.name,
+          });
+        },
+        focus: (event) => {
+          onFocus({ target: event.target });
+        },
+      },
     });
 
     const inputDisplayName = new Input({
-      name: "display_name",
-      label: "chat name",
-      type: "text",
+      ...display_name,
+      events: {
+        blur: (event) => {
+          onBlur({
+            target: event.target,
+            value: event.target.value,
+            input: display_name.name,
+          });
+        },
+        focus: (event) => {
+          onFocus({ target: event.target });
+        },
+      },
     });
 
     const inputPhone = new Input({
-      name: "phone",
-      label: "phone number",
-      type: "tel",
+      ...phone,
+      events: {
+        blur: (event) => {
+          onBlur({
+            target: event.target,
+            value: event.target.value,
+            input: phone.name,
+          });
+        },
+        focus: (event) => {
+          onFocus({ target: event.target });
+        },
+      },
     });
 
     super("div", {
@@ -63,6 +185,8 @@ class ProfileSetting extends Block {
       inputDisplayName,
       inputPhone,
       formButton,
+      ...inputsNames,
+      ...inputsLabels,
     });
   }
 
