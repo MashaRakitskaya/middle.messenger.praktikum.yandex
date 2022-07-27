5391b940df31c0ee838141156ef51e8c85312c8d
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-27 09:15:57 +0200

    change: rename constants SIGNIN_PATH ="/signin"-->"/"; export const MESSENGER_PATH ="/chats"--> "/messenger";

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,9 @@
 export const BASE_URL = window.origin;
-export const SIGNIN_PATH = "/signin";
+export const SIGNIN_PATH = "/";
 export const SIGNUP_PATH = "/signup";
 export const PROFILE_SETTING_PATH = "/profile-setting";
 export const PASSWORD_SETTING_PATH = "/password-setting";
-export const CHATS_PATH = "/chats";
+export const MESSENGER_PATH = "/messenger";
 export const ERROR404_PATH = "/404";
 export const ERROR500_PATH = "/500";
 export const ROOT_DIV = "#root";
