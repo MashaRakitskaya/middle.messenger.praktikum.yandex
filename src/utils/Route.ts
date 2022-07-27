fe7595513b3cf7d8758577cd335db9b522259d49
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-07-27 09:13:37 +0200

    add: Route class

---
+++ src/utils/Route.ts
@@ -0,0 +1,51 @@
+import { renderDom } from "./renderDOM";
+
+function isEqual(lhs, rhs) {
+  return lhs === rhs;
+}
+
+//Route хранит URL и соответствующий ему блок, умеет показывать, скрывать и создавать блоки.
+class Route {
+  private _pathname: any;
+  private _blockClass: any;
+  private _block: any;
+  private _props: any;
+  constructor(pathname, view, props) {
+    this._pathname = pathname;
+    this._blockClass = view;
+    this._block = null;
+    this._props = props;
+  }
+
+  //создавать блоки
+  navigate(pathname) {
+    if (this.match(pathname)) {
+      this._pathname = pathname;
+      this.render();
+    }
+  }
+
+  //скрывать
+  leave() {
+    if (this._block) {
+      this._block.remove();
+    }
+  }
+
+  match(pathname) {
+    return isEqual(pathname, this._pathname);
+  }
+
+  //отобразит(показывать) на экран нужный блок
+  render() {
+    if (!this._block) {
+      this._block = new this._blockClass();
+      renderDom(this._props.rootQuery, this._block);
+      return;
+    }
+
+    this._block.show();
+  }
+}
+
+export default Route;
