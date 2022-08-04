e3ef198655ce99065b9726f89579f68afa4b7176
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-08-04 14:35:09 +0200

    change: class ChatPage add addUsersButton, deleteUsersButton, deleteChatButton

--- src/modules/chatPage/chatPage.ts
+++ src/modules/chatPage/chatPage.ts
@@ -11,7 +11,7 @@ import MessageSubmitButton from "../../components/messageFormSubmitButton/messag
 import MessageInput from "../../components/messageInput/messageInput";
 
 class ChatPage extends Block {
-  constructor(props: Record<string, any> = {}) {
+  constructor({ addUsersButton, deleteUsersButton, deleteChatButton }) {
     const addErrorMessage = (message) => {
       const error = document.querySelector(".form-error") as HTMLElement;
       if (error) {
@@ -27,14 +27,6 @@ class ChatPage extends Block {
         error.textContent = "";
       }
     };
-
-    const avatar = new Avatar({
-      urlImg:
-        "https://images.unsplash.com/photo-1655269359642-caf63bad5a05?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80",
-      class: "chat-img",
-    });
-
-    const chatName = new ChatName({ chatName: "Elena List" });
 
     const messageInput = new MessageInput({
       events: {
@@ -61,15 +53,15 @@ class ChatPage extends Block {
     });
 
     super("div", {
-      ...props,
       messageContent:
         "quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore",
       urlImg:
         "https://images.unsplash.com/photo-1657013881676-f375a031a421?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      avatar,
-      chatName,
       messageSubmitButton,
       messageInput,
+      addUsersButton,
+      deleteUsersButton,
+      deleteChatButton,
     });
   }
 
