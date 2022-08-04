54e25a0c0aed70e63746f4991c5d9052af55e08c
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-08-04 14:14:22 +0200

    create: utils method addErrorMessage, showContextmenu, showPopup, hidePopup, merge, set

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -11,3 +11,88 @@ export function queryStringify(data: Record<string, any>) {
 export function isEqual(data1, data2) {
   return data1 === data2;
 }
+
+export const addErrorMessage = (message) => {
+  const error = document.querySelector(".form-error") as HTMLElement;
+  if (error) {
+    error.style.visibility = "visible";
+    error.textContent = message;
+  }
+};
+
+export const showContextmenu = ({ event, contextMenu }) => {
+  event.preventDefault();
+
+  const contextMenuElement = document.querySelector(
+    ".context-menu"
+  ) as HTMLElement;
+
+  const { clientX: mouseX, clientY: mouseY } = event;
+
+  const { normalizedX, normalizedY } = contextMenu.normalizePozition(
+    mouseX,
+    mouseY,
+    document.getElementById("sidebar")
+  );
+
+  if (contextMenuElement) {
+    contextMenuElement.classList.remove("visible");
+    contextMenuElement.style.top = `${normalizedY}px`;
+    contextMenuElement.style.left = `${normalizedX}px`;
+
+    setTimeout(() => {
+      contextMenuElement.classList.add("visible");
+    });
+  }
+};
+
+export const showPopup = ({ popupId }) => {
+  const avatar = document.getElementById(popupId) as HTMLElement;
+  avatar.classList.add("popup_opened");
+};
+
+export const hidePopup = (popup: HTMLElement) => {
+  popup.classList.remove("popup_opened");
+};
+
+type Indexed<T = any> = {
+  [key in string]: T;
+};
+
+function merge(lhs: Indexed, rhs: Indexed): Indexed {
+  for (let p in rhs) {
+    if (!rhs.hasOwnProperty(p)) {
+      continue;
+    }
+
+    try {
+      if (rhs[p].constructor === Object) {
+        rhs[p] = merge(lhs[p] as Indexed, rhs[p] as Indexed);
+      } else {
+        lhs[p] = rhs[p];
+      }
+    } catch (e) {
+      lhs[p] = rhs[p];
+    }
+  }
+
+  return lhs;
+}
+
+export function set(state: Indexed, path: string, value: any): Indexed {
+  if (typeof state !== "object" || state === null) {
+    return state;
+  }
+
+  if (typeof path !== "string") {
+    throw new Error("path must be string");
+  }
+
+  const result = path.split(".").reduceRight<Indexed>(
+    (acc, key) => ({
+      [key]: acc,
+    }),
+    value as any
+  );
+  return merge(state as Indexed, result);
+}
