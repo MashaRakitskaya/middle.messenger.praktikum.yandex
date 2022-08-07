f3cc64b4a65c3a00a01d64e711a0dd96f9260af6
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-08-07 21:25:24 +0200

    feature: userContraller

---
+++ src/utils/controllers/UserContraller.ts
@@ -0,0 +1,13 @@
+import user from "../api/users";
+import store from "../store";
+
+class UserContraller {
+  searchUserByLogin({ login }) {
+    user.searchUserByLogin({ login }).then((response) => {
+      store.set("foundUsersSearch", JSON.parse(response.response));
+    });
+  }
+}
+
+const userContraller = new UserContraller();
+export default userContraller;
