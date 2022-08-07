c3da9681bb1abec2dc3da7ff31b5842a37f9dbeb
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-08-07 21:35:30 +0200

    changes: class Popup

--- src/modules/popup/popup.ts
+++ src/modules/popup/popup.ts
@@ -3,34 +3,63 @@ import popupTemplate from "./popup.hbs";
 import Block from "../../utils/Block";
 import PageTitle from "../../components/pageTitle/pageTitle";
 
+interface PopupProps {
+  isPopupAddUsersToChat?;
+  formId;
+  popupId;
+  nameInput;
+  labelInput;
+  input?;
+  pageTitleText;
+  popupFormButtonAdd?;
+  popupFormButtonSearch?;
+  setFoundUsers?;
+  foundUsers?;
+}
+
 class Popup extends Block {
   constructor({
+    isPopupAddUsersToChat,
     formId,
     popupId,
     nameInput,
     labelInput,
     input,
     pageTitleText,
-    popupFormButton,
-  }) {
+    popupFormButtonAdd,
+    popupFormButtonSearch,
+    setFoundUsers,
+    foundUsers,
+  }: PopupProps) {
     const pageTitle = new PageTitle({
       pageTitle: pageTitleText,
     });
 
     super("div", {
+      isPopupAddUsersToChat,
       formId,
       popupId,
       nameInput,
       labelInput,
       input,
       pageTitle,
-      popupFormButton,
+      popupFormButtonAdd,
+      popupFormButtonSearch,
+      setFoundUsers,
+      foundUsers,
       events: {
         click: (event) => {
           const avatar = document.getElementById(popupId) as HTMLElement;
+          const formElement = document.getElementById(
+            formId
+          ) as HTMLFormElement;
           if (event.target.classList.contains("popup")) {
             avatar.classList.remove("popup_opened");
+            if (isPopupAddUsersToChat) {
+              this.setProps({ setFoundUsers: false });
+            }
           }
+          formElement.reset();
         },
       },
     });
