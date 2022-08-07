5fb90544e642d853f280512e8d8e48261e44386e
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-08-07 21:41:52 +0200

    feature: class  FoundUsersDelete, and class FoundUsersSearch

---
+++ src/components/foundUsers/foundUsersDelete.ts
@@ -0,0 +1,57 @@
+import foundUsersSearchTemplate from "./foundUsers.hbs";
+import Block from "../../utils/Block";
+import store, { StoreEvents } from "../../utils/store";
+import FoundUser from "../foundUser/foundUser";
+import chats from "../../utils/api/chats";
+import { hidePopup } from "../../utils/utils";
+import { popupIds } from "../../utils/constants";
+
+class FoundUsersDelete extends Block {
+  constructor() {
+    super("div");
+
+    store.on(StoreEvents.Updated, () => {
+      const { foundUsersDelete } = store.getState();
+
+      const changedFoundUsersSearch: any = [];
+
+      foundUsersDelete?.forEach((element: any) => {
+        changedFoundUsersSearch.push(
+          new FoundUser({
+            id: element.id,
+            login: element.login,
+            second_name: element.second_name,
+            events: {
+              click: () => {
+                const popup = document.getElementById(
+                  popupIds.idPopupDeleteUsers
+                ) as HTMLElement;
+
+                const chatPage = document.getElementById(
+                  "chat-page"
+                ) as HTMLElement;
+
+                const chatId = chatPage.getAttribute("data-id");
+                chats
+                  .deleteUsersFromChat({ users: [element.id], chatId: chatId })
+                  .then((response) => {
+                    if (response.status === 200) {
+                      hidePopup(popup);
+                    }
+                  });
+              },
+            },
+          })
+        );
+      });
+
+      this.setProps({ changedFoundUsersSearch: changedFoundUsersSearch });
+    });
+  }
+
+  render() {
+    return this.compile(foundUsersSearchTemplate, this.props);
+  }
+}
+
+export default FoundUsersDelete;
---
+++ src/components/foundUsers/foundUsersSearch.ts
@@ -0,0 +1,57 @@
+import foundUsersSearchTemplate from "./foundUsers.hbs";
+import Block from "../../utils/Block";
+import store, { StoreEvents } from "../../utils/store";
+import FoundUser from "../foundUser/foundUser";
+import chats from "../../utils/api/chats";
+import { hidePopup } from "../../utils/utils";
+import { popupIds } from "../../utils/constants";
+
+class FoundUsersSearch extends Block {
+  constructor() {
+    super("div");
+
+    store.on(StoreEvents.Updated, () => {
+      const { foundUsersSearch } = store.getState();
+
+      const changedFoundUsersSearch: any = [];
+
+      foundUsersSearch?.forEach((element: any) => {
+        changedFoundUsersSearch.push(
+          new FoundUser({
+            id: element.id,
+            login: element.login,
+            second_name: element.second_name,
+            events: {
+              click: () => {
+                const popup = document.getElementById(
+                  popupIds.idPopupAddUsers
+                ) as HTMLElement;
+
+                const chatPage = document.getElementById(
+                  "chat-page"
+                ) as HTMLElement;
+
+                const chatId = chatPage.getAttribute("data-id");
+                chats
+                  .addUsersToChat({ chatId: chatId, users: [`${element.id}`] })
+                  .then((response) => {
+                    if (response.status === 200) {
+                      hidePopup(popup);
+                    }
+                  });
+              },
+            },
+          })
+        );
+      });
+
+      this.setProps({ changedFoundUsersSearch: changedFoundUsersSearch });
+    });
+  }
+
+  render() {
+    return this.compile(foundUsersSearchTemplate, this.props);
+  }
+}
+
+export default FoundUsersSearch;
