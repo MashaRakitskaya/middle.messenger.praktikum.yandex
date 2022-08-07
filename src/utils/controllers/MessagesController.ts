a738a9a94d83e14999b1eaa3bc93437cd5120db2
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-08-07 21:25:46 +0200

    feature: messagesController

---
+++ src/utils/controllers/MessagesController.ts
@@ -0,0 +1,18 @@
+import store from "../store";
+
+interface MessageProps {
+  content: string;
+  time: string;
+  user_id: number;
+  type: string;
+}
+class MessagesController {
+  addMessage(message: MessageProps) {
+    const { messages = [] } = store.getState();
+    messages.push(message);
+    store.set("messages", messages);
+  }
+}
+
+const messagesController = new MessagesController();
+export default messagesController;
