e29582895cb927d21134dfd15135685d03f8d9a5
Masha Rakitskaya <masha.rakitskaya.97@yandex.ru>
2022-08-07 21:26:08 +0200

    feature: chatsController

---
+++ src/utils/controllers/ChatsController.ts
@@ -0,0 +1,29 @@
+import chats from "../api/chats";
+import store from "../store";
+import { hidePopup } from "../utils";
+
+class ChatsController {
+  getChats() {
+    return chats.getChats().then((response) => {
+      store.set("chats", JSON.parse(response.response));
+    });
+  }
+
+  createChat({ title, popup }) {
+    chats.createChat({ title }).then((response) => {
+      this.getChats();
+      if (response.status === 200) {
+        hidePopup(popup);
+      }
+    });
+  }
+
+  deleteChat({ chatId }) {
+    chats.deleteChat({ chatId }).then(() => {
+      this.getChats();
+    });
+  }
+}
+
+const chatsController = new ChatsController();
+export default chatsController;
